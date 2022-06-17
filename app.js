8d6de2b568451db51f0f4db6d694edad07aefb76
wea9677 <wea9677@naver.com>
2022-06-17 21:49:41 +0900

    router 추가 및 DB 로컬 테스트

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const port = 8080;
 const cors = require('cors');
+const userRouter = require("./routes/user");
 // const marketRouter = require("./routes/market");
 
 
-
-mongoose.connect("mongodb+srv://test:sparta@cluster0.l2ux3.mongodb.net/THUNDERMARKET?retryWrites=true&w=majority", {
+mongoose.connect('mongodb://0.0.0.0/cloneporject', {
+// mongoose.connect("mongodb+srv://test:sparta@cluster0.l2ux3.mongodb.net/THUNDERMARKET?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -21,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 
-app.use('/api', express.urlencoded({ extended: false }),);
+app.use('/api', express.urlencoded({ extended: false }),[userRouter]);
 
 app.use('/', (req, res) =>{
     res.send('클론코딩 테스트 페이지')
