8e40799aa7638a09ef2ca59c2239deca3f0037b1
wea9677 <wea9677@naver.com>
2022-06-17 18:06:29 +0900

    app.js 기본 모델

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const port = 8080;
+const cors = require('cors');
+// const marketRouter = require("./routes/market");
+
+
+
+mongoose.connect("mongodb+srv://test:sparta@cluster0.l2ux3.mongodb.net/THUNDERMARKET?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.use('/api', express.urlencoded({ extended: false }),);
+
+app.use('/', (req, res) =>{
+    res.send('클론코딩 테스트 페이지')
+});
+
+
+app.listen(port, () => {
+    console.log(port, '포트로 서버가 켜졌어요!')
+});
