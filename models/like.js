97114751cfd531022ba18aa565aa30a07d98c2a3
jthebat <taein007@gmail.com>
2022-06-18 09:31:33 +0900

    FEAT: like API , model 생성

---
+++ models/like.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const LikeSchema = new mongoose.Schema({    
+    //email 도 unique 한 값이기 때문에 email을 쓰려고 했으나 개발자 도구에서 payload에 email이 노출되는것은 안좋다고 생각하여 다시변경
+    userId: {
+        type: String,
+        required : true,
+    },
+    itemId: {
+        type: String,
+        required: true,
+    }
+},
+    //{ timestamps: true } 좋아요에 필요한가 의문
+);
+LikeSchema.virtual('likeId').get(function () {
+    return this._id.toHexString();
+    });
+LikeSchema.set("toJSON", { virtuals: true });
+
+module.exports = mongoose.model('Like', LikeSchema);
---
+++ routes/like.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth-middleware');
+const router = express.Router();
+
+const Like = require('../models/like');
+
+
+router.get('/like/:itemId', async (req, res) => {
+    const { itemId } = req.params;
+    const totalLike = await Like.find({itemId}).exec();
+    res.status(200).json(totalLike);
+});
+
+router.post('/like/:itemId',authMiddleware ,async (req, res) => {
+    try{                      
+        const {itemId} = req.params;
+        const { userId } = res.locals.user;       
+        
+        const findLike = await Like.findOne({ itemId,userId }).exec();
+        if(findLike){          
+            //return res.status(400).send({errorMessage: "이미 좋아요를 누름"})//테스트 메세지
+            return res.status(400).send({errorMessage: "fail"});
+        }
+        await Like.create({            
+            itemId,
+            userId,
+          });    
+          //return res.status(201).json({ like, msg: "좋아요 완료!" });     //테스트 메세지
+          return res.status(201).json({result: "success" });
+      
+    } catch (error) {    
+        res.status(400).send({
+        errorMessage: '좋아요 선택 실패하였습니다.',
+        });
+    }
+    
+});
+router.delete('/like/cancel/:itemId',authMiddleware ,async (req, res)=> {
+    const { itemId } = req.params;
+    const { userId } = res.locals.user;
+  
+    const findLike = await Like.findOne({ itemId,userId }).exec();
+  
+    if (!findLike) {
+      //return res.status(40).send({ errorMessage: "좋아요를 찾을 수 없습니다." }); //메스트 메세지
+      return res.status(400).send({ errorMessage: "fail" });
+    }
+  
+    await Like.deleteOne(findLike);
+    return res.status(200).json({ result: "success" });
+});
+
+
+module.exports = router;
+
