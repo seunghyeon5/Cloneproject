5ab632925bc0083ef287f0fcdc03323ba451d849
wea9677 <wea9677@naver.com>
2022-06-21 14:13:32 +0900

    cors 와일드카드

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(express.json());
 
 app.use(cors({ // CORS 모듈 실행
     //origin : "http://clonebunjang.s3-website.ap-northeast-2.amazonaws.com",  
-    origin : "http://localhost:3000", // 출처 허용 옵션 (전부 허용)
+    origin : "*", // 출처 허용 옵션 (전부 허용)
     credential: 'true' // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
 }));
 
